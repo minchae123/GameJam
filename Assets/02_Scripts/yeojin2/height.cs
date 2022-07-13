6fc91553ea4d9d177a89edfdcd4cd6e14742190f
ye0jin <fruitfighterlim@gmail.com>
2022-07-13 12:54:37 +0900

    몸무게 / 인트로

---
+++ Assets/02_Scripts/yeojin2/height.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class height : MonoBehaviour
+{
+    public float Height  = 75.0f;
+    public Text heighttext;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Height -= Time.deltaTime *0.1f;
+        heighttext.text = " " + Height.ToString("F2");
+    }
+}
